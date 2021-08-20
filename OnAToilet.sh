59678f90d2bca06416f386f6b85fd5cd20a92d09
BerBerOnGithub <86917016+BerBerOnGithub@users.noreply.github.com>
2021-08-20 13:11:32 +0200

    Add files via upload

---
+++ OnAToilet.sh
@@ -0,0 +1,6 @@
+sudo apt install toilet
+sudo yum install toilet
+sudo dnf install toilet
+sudo pacman -S toilet
+sudo pkg_add -v toilet
+toilet -f mono12 -F metal BERBERONGITHUB INSTALLED THIS ONTO YOUR OS.
